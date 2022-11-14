4a3ee92b44d72b1713c8a9a2f139ecb04e0966e3
I516965 <abdullah.karatas@sap.com>
2022-11-14 11:57:09 +0100

    change with path

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import glutenfreeyufka from "../../Assets/Basics/glutenfreeyufka.png";
-import lentilpatties from "../../Assets/Basics/lentis-patties.jpg";
-import ramen from "../../Assets/Asian-Food/ramen.png";
-import broccolipakorakadhi from "../../Assets/Indian-Food/broccoli-pakora-kadhi.png"
 import recipes from "../../recipes/recipes.json";
 
 const asiancuisine = recipes.main.recipe.asiancuisine;
+const basiccuisine = recipes.main.recipe.basics;
+const indiancuisine = recipes.main.recipe.indian;
 
 function Projects() {
   return (
@@ -19,27 +17,20 @@ function Projects() {
           <strong className="purple">Basics </strong>
         </h1>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={glutenfreeyufka}
-              isBlog={false}
-              title="Gluten-free Yufka"
-              description="Ein schneller und einfacher glutenfreier Yufka. Mit der geheimen Zusammensetzung für garantierten Erfolg."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={lentilpatties}
-              isBlog={false}
-              title="Lentil patties"
-              description="Einfache, schnelle und gesunde Linsen patties. Erweiterbar mit Gemüse deiner Wahl. Passt hervorragend zu einem leckeren Salat."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+          {basiccuisine.map((value, index) => (
+            <Col md={4} className="project-card">
+              {console.log(value.imagepath)}
+              <ProjectCard
+                imgPath={value.imagepath}
+                isBlog={false}
+                title={value.title}
+                description={value.smallDescription}
+                ghLink="https://github.com/soumyajit4419/Chatify"
+                demoLink="https://chatify-49.web.app/"
+              />
+            </Col>
+            ))
+          }
         </Row>
       </Container>
       <Container>
@@ -50,7 +41,7 @@ function Projects() {
         {asiancuisine.map((value, index) => (
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={ramen}
+              imgPath={value.imagepath}
               isBlog={false}
               title={value.title}
               description={value.smallDescription}
@@ -67,16 +58,19 @@ function Projects() {
           <strong className="purple">Indian Food </strong>
         </h1>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={broccolipakorakadhi}
-              isBlog={false}
-              title="Broccoli Pakora Kadhi"
-              description="Gesund oder ungesund ist hier die Frage. Ob airfryer, backofen oder in der Fritteuse, mit diesem Gericht wird die Kombination aus Kichererbsen und Brokkoli zum Traum."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
+          {indiancuisine.map((value, index) => (
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={value.imagepath}
+                  isBlog={false}
+                  title={value.title}
+                  description={value.smallDescription}
+                  ghLink="https://github.com/soumyajit4419/Chatify"
+                  demoLink="https://chatify-49.web.app/"
+                />
+              </Col>
+            ))
+          }
         </Row>
       </Container>
     </Container>
