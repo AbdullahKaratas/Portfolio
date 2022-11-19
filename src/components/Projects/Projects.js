abacc6942eb6d5c0958a054e798f6eac3c673805
AbdullahKaratas <abdullah.karatas@icloud.com>
2022-11-19 09:23:42 +0100

    Update Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,8 @@ import recipes from "../../recipes/recipes.json";
 const asiancuisine = recipes.main.recipe.asiancuisine;
 const basiccuisine = recipes.main.recipe.basics;
 const indiancuisine = recipes.main.recipe.indian;
+const italiancuisine = recipes.main.recipe.italian;
+const spreadscuisine = recipes.main.recipe.spreads;
 
 function Projects() {
   return (
@@ -58,6 +60,46 @@ function Projects() {
         </h1>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           {indiancuisine.map((value, index) => (
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={value.imagepath}
+                  isBlog={false}
+                  title={value.title}
+                  description={value.smallDescription}
+                  ingredients={value.ingredients}
+                  steps={value.steps}
+                />
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+      <Container>
+        <h1 className="project-heading">
+          <strong className="purple">Italian Food </strong>
+        </h1>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          {italiancuisine.map((value, index) => (
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={value.imagepath}
+                  isBlog={false}
+                  title={value.title}
+                  description={value.smallDescription}
+                  ingredients={value.ingredients}
+                  steps={value.steps}
+                />
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+      <Container>
+        <h1 className="project-heading">
+          <strong className="purple">Spreads</strong>
+        </h1>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          {spreadscuisine.map((value, index) => (
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={value.imagepath}
